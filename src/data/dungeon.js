5fc23c87cc92413f5d851e2fbf7e211fcd4912c2
Robert Burnham <burnhamrobertp@gmail.com>
2018-05-05 22:22:07 -0500

    Remove HP from DungeonFeatures

--- src/data/dungeon.js
+++ src/data/dungeon.js
@@ -4,7 +4,6 @@ export const DungeonFeatures = {
   WALL: {
     INDESTRUCTIBLE: {
       name: DungeonNames.wall_i,
-      hp: -1,
       sprite: {
         class: 'stone-dark',
         variants: 4,
@@ -14,7 +13,6 @@ export const DungeonFeatures = {
   FLOOR: {
     DIRT_GREY: {
       name: DungeonNames.floor_dirt_grey,
-      hp: -1,
       sprite: {
         class: 'floor-grey-dirt',
         variants: 8,
