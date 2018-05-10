b9310d8fbd0016c7201bce1025e5115d2de4164a
Robert Burnham <burnhamrobertp@gmail.com>
2018-05-09 21:54:49 -0500

    Default to male player sprites until I can handle gender

--- src/data/player.js
+++ src/data/player.js
@@ -6,37 +6,37 @@ export const PlayerRaces = {
   dwarf: {
     name: RaceNames.dwarf,
     sprite: {
-      class: 'player-dwarf',
+      class: 'player-dwarf-m',
     },
   },
   elf: {
     name: RaceNames.elf,
     sprite: {
-      class: 'player-elf',
+      class: 'player-elf-m',
     },
   },
   gnome: {
     name: RaceNames.gnome,
     sprite: {
-      class: 'player-gnome',
+      class: 'player-gnome-m',
     },
   },
   human: {
     name: RaceNames.human,
     sprite: {
-      class: 'player-human',
+      class: 'player-human-m',
     },
   },
   orc: {
     name: RaceNames.orc,
     sprite: {
-      class: 'player-orc',
+      class: 'player-orc-m',
     },
   },
   troll: {
     name: RaceNames.troll,
     sprite: {
-      class: 'player-troll',
+      class: 'player-troll-m',
     },
   },
 }
