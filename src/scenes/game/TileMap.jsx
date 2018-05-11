e703bb36089b6ef1bdb9acd9ae4e77bc6061275f
Robert Burnham <burnhamrobertp@gmail.com>
2018-05-11 11:38:41 -0500

    Clean up TileMap's long arrow function

--- src/scenes/game/TileMap.jsx
+++ src/scenes/game/TileMap.jsx
@@ -4,11 +4,15 @@ import TileRow from './components/TileRow'
 
 class TileMap extends React.Component {
   render() {
-    const tiles = this.props.map.map((tiles, y) => <TileRow key={y} y={y} tiles={tiles} />)
+    const tiles = this.props.map.map(this.renderRow)
 
     return <div id="game-map">
       {tiles}
     </div>
+  }
+
+  static renderRow(tiles, yIndex) {
+    return <TileRow key={yIndex} y={yIndex} tiles={tiles} />
   }
 }
 
