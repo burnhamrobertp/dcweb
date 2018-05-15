49f470d5bf688f3aa43fb47fa28bcb3bf03c9c1e
Robert Burnham <burnhamrobertp@gmail.com>
2018-05-14 22:38:50 -0500

    Excess console.log left in

--- src/scenes/game/components/Tile.jsx
+++ src/scenes/game/components/Tile.jsx
@@ -5,7 +5,6 @@ import { getTileFeatures, makeGetTileFeatures } from '../../../state/selectors'
 
 class Tile extends React.Component {
   render() {
-    console.log(this.props.x, this.props.y)
     const features = this.props.features.map((feature, i) => <Feature key={i} feature={feature} />)
     return <div className={this.props.tile.sprite.class}>
       {features}
