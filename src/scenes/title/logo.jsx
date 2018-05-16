ece8e16f32bb68a3db8fd8c9904091d1879c548d
Robert Burnham <burnhamrobertp@gmail.com>
2018-05-16 01:28:47 -0500

    Remove extraneous console.log

--- src/scenes/title/logo.jsx
+++ src/scenes/title/logo.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 class Logo extends React.Component {
   render() {
-    console.log(process.env);
     return <div id="logo">
       <div>{`   ..                                                     ..
  dF                     x=~                         . uW8"
